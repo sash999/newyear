d0ea32aa0744ce1657c84701fb75dd204e4c92ab
sash999 <sash999@yandex.ru>
2018-11-15 13:01:33 +0300

    initial commit

---
+++ newyear.ino
@@ -0,0 +1,57 @@
+
+int pins[] = {6,9,10,11};
+int led_up;
+int led_down;
+
+#define STEP 5
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(11,OUTPUT);
+}
+
+void light_up(int pin, int step) {
+  for(int i=0; i<=255; i++)
+    {
+    analogWrite(pin,i);
+    delay(step);
+    }
+}
+
+void light_down(int pin, int step) {
+  for(int i=255; i>=0; i--)
+    {
+    analogWrite(pin,i);
+    delay(step);
+    }
+}    
+
+void LightUpDown(int pin_up, int pin_down, int step) {
+  for(int i=0; i<=255; i++) {
+  analogWrite(pin_up,i);
+  analogWrite(pin_down, 255-i);
+  delay(step);
+}
+}
+
+
+
+void loop() {
+ 
+for(int j =0; j<=3; j++) { 
+ 
+ if(j == 0) {
+  led_up = pins[0];
+  led_down = pins[3];
+  }
+ else {
+  led_up = pins[j];
+  led_down = pins[j-1];
+ }
+  LightUpDown(led_up, led_down,STEP);
+ 
+}
+  
+}
